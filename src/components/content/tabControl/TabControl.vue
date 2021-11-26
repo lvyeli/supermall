efe0cdf609f5bb2e621bad1cc9ceb5159c2a145c
lvyali <764119434@qq.com>
2021-11-26 19:29:36 +0800

    message

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tab-control">
+    <div class="tab-control-item" 
+        v-for="(item, index) in titles" :key="index"
+        :class="{active: index === currentIndex}"
+        @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+      this.$emit('tabClick', index)
+    }
+  }
+
+}
+</script>
+
+<style>
+  .tab-control {
+    display: flex;
+    justify-content: space-around;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+    background-color: #fff;
+  }
+  .active span {
+    color: var(--color-high-text);
+    border-bottom: 2px solid red;
+    padding-bottom: 5px;
+  }
+</style>
